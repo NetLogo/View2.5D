a4367abfd6313ebdc572d70812f77730a65d06f6
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2021-08-25 09:20:11 -0500

    Avoid null ref errors when running headlessly
    This extension doesn't do much headlessly, but at least avoid the errors
    if it happens during automated tests or packaging.

--- src/main/java/view25d/view/View25DShapeChangeListener.scala
+++ src/main/java/view25d/view/View25DShapeChangeListener.scala
@@ -1,19 +1,28 @@
 // (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
 
 package view25d.view
+
+import java.awt.GraphicsEnvironment
+import java.util.Objects
+
 import view25d.View25DExtension
 import org.nlogo.app.App
 import org.nlogo.core.ShapeEvent
 
 object View25DShapeChangeListener {
-  def listen() {
-    val turtleShapeTracker = App.app.workspace.world.turtleShapes
+  def isHeadless: Boolean =
+    GraphicsEnvironment.isHeadless || Objects.equals(System.getProperty("org.nlogo.preferHeadless"), "true")
 
-    val turtleListener = new turtleShapeTracker.Sub {
-      def notify(pub: turtleShapeTracker.Pub, event: ShapeEvent): Unit = {
-        View25DExtension.updateTurtleShapesAllViews
+  def listen() {
+    if (!isHeadless) {
+      val turtleShapeTracker = App.app.workspace.world.turtleShapes
+
+      val turtleListener = new turtleShapeTracker.Sub {
+        def notify(pub: turtleShapeTracker.Pub, event: ShapeEvent): Unit = {
+          View25DExtension.updateTurtleShapesAllViews
+        }
       }
+      turtleShapeTracker.subscribe(turtleListener)
     }
-    turtleShapeTracker.subscribe(turtleListener)
   }
 }
